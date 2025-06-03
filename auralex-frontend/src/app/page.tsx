4e9b5c01a82970aad66bb9c3bbda06a6364198ab
EDOHWARES <edohemmanuel4real@gmail.com>
2025-06-03 16:47:57 +0100

    feat/benefit-section

--- auralex-frontend/src/app/page.tsx
+++ auralex-frontend/src/app/page.tsx
@@ -1,16 +1,17 @@
-"use client"
-import React from 'react';
-import HeroSection from '@/components/sections/HeroSection';
-import FeaturesSection from '@/components/sections/FeaturesSection';
+"use client";
+import React from "react";
+import HeroSection from "@/components/sections/HeroSection";
+import FeaturesSection from "@/components/sections/FeaturesSection";
+import BenefitsSection from "@/components/sections/BenefitsSection";
 
 const HomePage = () => {
   return (
     <main className="min-h-screen">
       <HeroSection />
       <FeaturesSection />
-
+      <BenefitsSection />
     </main>
-  )
-}
+  );
+};
 
 export default HomePage;
